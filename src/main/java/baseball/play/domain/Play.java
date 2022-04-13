7a94676920b2d6f81a0158920dccd3e9fe300da8
Sung-jin <osj4872@gmail.com>
2022-04-13 19:29:20 +0900

    feat: 사용자의 입력 기능 및 입력 유효 검사 기능을 담당하는 클래스 구현

---
+++ src/main/java/baseball/play/domain/Play.java
@@ -0,0 +1,64 @@
+package baseball.play.domain;
+
+import baseball.util.CheckerUtil;
+import baseball.util.GeneratorUtil;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Play {
+    private final int ANSWER_LENGTH = 3;
+    private final int ANSWER_RANGE_START_NUMBER = 1;
+    private final int ANSWER_RANGE_END_NUMBER = 9;
+
+    private List<Integer> answer;
+    private Boolean isEndedGame;
+    private int ballCount = 0;
+    private int strikeCount = 0;
+
+    public void start() {
+        init();
+
+        while (!isEndedGame) {
+            List<Integer> enteredAnswer = enterAnswerByPlayer();
+        }
+    }
+
+    private void init() {
+        answer = GeneratorUtil.generateRandomIntegerListWithoutDuplicate(
+            ANSWER_RANGE_START_NUMBER, ANSWER_RANGE_END_NUMBER, ANSWER_LENGTH
+        );
+        isEndedGame = false;
+    }
+
+    private List<Integer> enterAnswerByPlayer() {
+        String enteredByPlayer = Console.readLine();
+
+        validateEnteredByPlayer(enteredByPlayer);
+
+        return convertStringToSingleNumberList(enteredByPlayer);
+    }
+
+    private void validateEnteredByPlayer(String value) {
+        if (CheckerUtil.hasExcludeNumberInString(value)) {
+            throw new IllegalArgumentException("Only numeric input is allowed");
+        }
+        if (value.length() != ANSWER_LENGTH) {
+            throw new IllegalArgumentException(String.format("You must enter a %d digit number", ANSWER_LENGTH));
+        }
+        if (CheckerUtil.hasDuplicateCharacterInString(value)) {
+            throw new IllegalArgumentException("Only non-duplicate numbers are allowed");
+        }
+    }
+
+    private List<Integer> convertStringToSingleNumberList(String value) {
+        List<Integer> result = new ArrayList<>();
+
+        for (String charValue: value.split("")) {
+            result.add(Integer.parseInt(charValue));
+        }
+
+        return result;
+    }
+}
