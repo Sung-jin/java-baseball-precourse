f8196f6b13729680b874a7d7f13fbd33f7028fb4
Sung-jin <osj4872@gmail.com>
2022-04-14 19:06:11 +0900

    feat: 정답 비교 기능 및 게임 완료 여부 선택 기능 구현

--- src/main/java/baseball/play/domain/Play.java
+++ src/main/java/baseball/play/domain/Play.java
@@ -7,6 +7,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Play {
     private final int ANSWER_LENGTH = 3;
@@ -22,7 +23,12 @@ public class Play {
         init();
 
         while (!isEndedGame) {
+            System.out.println("숫자를 입력해주세요 : ");
             List<Integer> enteredAnswer = enterAnswerByPlayer();
+
+            checkAnswer(enteredAnswer);
+            checkResult();
+            countInit();
         }
     }
 
@@ -51,5 +57,66 @@ public class Play {
         if (CheckerUtil.hasDuplicateCharacterInString(value)) {
             throw new IllegalArgumentException("Only non-duplicate numbers are allowed");
         }
+    }
+
+    private void checkAnswer(List<Integer> enteredAnswer) {
+        List<Integer> correctValues = LoopHelper.listFilter(enteredAnswer, (value) -> answer.contains(value));
+
+        LoopHelper.listForEach(correctValues, (value) -> {
+            int answerIndex = answer.indexOf(value);
+            int enteredAnswerIndex = enteredAnswer.indexOf(value);
+
+            if (answerIndex == enteredAnswerIndex) {
+                strikeCount++;
+            }
+
+            if (answerIndex != enteredAnswerIndex) {
+                ballCount++;
+            }
+        });
+    }
+
+    private void checkResult() {
+        List<String> result = new ArrayList<>();
+
+        if (ballCount > 0) {
+            result.add(String.format("%d볼", ballCount));
+        }
+
+        if (strikeCount > 0) {
+            result.add(String.format("%d스트라이크", strikeCount));
+        }
+
+        if (ballCount == 0 && strikeCount == 0) {
+            result.add("낫싱");
+        }
+
+        System.out.println(String.join(" ", result));
+
+        if (strikeCount == ANSWER_LENGTH) {
+            System.out.printf("%d개의 숫자를 모두 맞히셨습니다! 게임 종료%n", ANSWER_LENGTH);
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            checkRestartPlay();
+        }
+    }
+
+    private void checkRestartPlay() {
+        String input = Console.readLine();
+
+        if (input.equals("1")) {
+            init();
+            return;
+        }
+        if (Objects.equals(input, "2")) {
+            isEndedGame = true;
+            return;
+        }
+
+        throw new IllegalArgumentException("You must enter 1 or 2");
+    }
+
+    private void countInit() {
+        strikeCount = 0;
+        ballCount = 0;
     }
 }
